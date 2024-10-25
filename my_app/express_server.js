b9a5868a3cac5e763bcab109c8516298cb094a1a
cgw <cgw@exciform.com>
2024-10-25 17:55:47 +0900

    first commit

---
+++ my_app/express_server.js
@@ -0,0 +1,101 @@
+import express from 'express';
+import nunjucks from 'nunjucks';
+import bodyParser from 'body-parser';
+import fs from "fs";
+import path from "path";
+import mongoose from 'mongoose';
+
+const __dirname = path.resolve();
+
+const app = express();
+
+// file path
+const filePath = path.join(__dirname, "data", "writing.json"); // my_app/data/writing.json
+
+// body parser set
+app.use(bodyParser.urlencoded({ extended: false })); // express 기본 모듈 사용
+app.use(bodyParser.json());
+
+// view engine set
+app.set('view engine', 'html'); // main.html -> main(.html)
+
+// nunjucks setting
+nunjucks.configure('views', {
+    watch: true, // html 파일이 수정될 경우, 다시 반영 후 렌더링
+    express: app // express가 어떤 객체를 나타내는지
+});
+
+// mongoose connect
+mongoose.connect("mongodb://127.0.0.1:27017").then(() => console.log("DB 연결 성공")).catch((e) => console.error(e));
+
+// mongoose setting
+const { Schema } = mongoose;
+
+const WritingSchema = new Schema({
+    title: String,
+    contents: String,
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const Writing = mongoose.model("Writing", WritingSchema);
+
+// middleware : request와 response 사이 뭔가를 하게함
+// main page GET
+app.get('/', async (req, res) => {
+    const fileData = fs.readFileSync(filePath);
+    const writings = JSON.parse(fileData);
+
+    res.render('main', { list: writings }); // nunjucks setting으로 views를 했기 때문에
+});
+
+app.get('/write', (req, res) => {
+    res.render('write');
+});
+
+app.post('/write', async (req, res) => {
+    const title = req.body.title;
+    const contents = req.body.contents;
+    //const date = req.body.date;
+
+    // 데이터 저장
+    // data/writing.json 안에 글 내용이 저장
+    // readFile 사용하지 않고 readFileSync 사용 이유 : node.js 자체가 비동기이기 때문.. 
+    /* const fileData = fs.readFileSync(filePath); // 파일 읽기
+
+    const writings = JSON.parse(fileData); // 파일 변환
+
+    // request 데이터를 저장
+    writings.push({
+        title: title,
+        contents: contents,
+        date: date,
+    });
+
+    // data/writing.json에 저장하기
+    fs.writeFileSync(filePath, JSON.stringify(writings)); */
+
+    // mongodb에 저장
+    const writing = new Writing({
+        title: title,
+        contents: contents
+    });
+    const result = await writing.save().then(() => {
+        console.log("Success");
+        res.render('detail', { 'detail': { title: title, contents: contents, /* date: date */ } });
+    }).catch((err) => {
+        console.error(err);
+        res.render("write");
+    });
+
+});
+
+app.get('/detail', async (req, res) => {
+    res.render('detail');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running');
+});
